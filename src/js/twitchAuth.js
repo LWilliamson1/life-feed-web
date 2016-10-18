8e1fc9a62183c1dd094abe963106d2000842abd6
LWilliamson1 <larry86_06@hotmail.com>
2016-10-17 23:02:17 -0400

    added clickable preview to append vid - next create a theater mode for added video and swap with currently playing vid

--- src/js/twitchAuth.js
+++ src/js/twitchAuth.js
@@ -2,6 +2,13 @@
   // Initialize. If we are already logged in, there is no
   // need for the connect button
 
+    var loadVideo = function(event) {
+      console.log(event.target.id);
+      
+      $("#twitch").append('<div class="col-xs-12" style="padding-bottom:1rem"><div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="http://player.twitch.tv/?autoplay=false&channel='+event.target.id+'" allowfullscreen="true" </iframe></div></div>');
+      
+    }
+
     Twitch.init({clientId: 'sse4niadmptvtv0pcpupi3lpbuzwlt9'}, function(error, status) {
         if (status.authenticated) {
           // we're logged in :)
@@ -21,11 +28,11 @@
         Twitch.api({method: 'streams/followed'}, function(error, user){
           console.log('display streams');
           //$("#results").append("<div>");
-          $.each(user.streams, function(index, item) {
-            $("#twitch").append('<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2" style="padding-bottom:1rem"><div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="http://player.twitch.tv/?autoplay=false&channel='+item.channel.name+'" allowfullscreen="true" </iframe></div></div>');
+//          $.each(user.streams, function(index, item) {
+//            $("#twitch").append('<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2" style="padding-bottom:1rem"><div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="http://player.twitch.tv/?autoplay=false&channel='+item.channel.name+'" allowfullscreen="true" </iframe></div></div>');
         
-//        $.each(user.streams, function(index, item) {
-//            $("#twitch").append('<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2"> <img class="images-responsive img-fluid" src="'+item.preview.large+'"></img></div>');
+        $.each(user.streams, function(index, item) {
+            $("#twitch").append('<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2"> <img id="'+item.channel.display_name+'" class="images-responsive img-fluid" onclick=loadVideo(event) src="'+item.preview.large+'"></img></div>');
               
 //              <img class="images-responsive img-fluid" src="'+item.preview.large+'"></img>
 //              '<iframe src="http://player.twitch.tv/?autoplay=false&channel='+item.channel.name+'" height="720" width="1280" frameborder="0"  scrolling="no" allowfullscreen="trues" autoplay="false"></iframe>'
@@ -57,6 +64,8 @@
 	Twitch.api({method: '/users/'+name+'/follows/channels', verb:'GET'}, function(error, user){
 		console.log(user);
 	});
+          
+
 	
 	Twitch.api({method: 'streams/followed'}, function(error, user){
 		console.log(user);
